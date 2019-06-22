140f230d881275aa067f2f62ff64fd0c55bd001a
Catherine <katiiforever@gmail.com>
2019-06-21 20:11:15 -0400

    adding Javascript

---
+++ script.js
@@ -0,0 +1,22 @@
+const celeste = document.getElementById('celeste')
+const violeta = document.getElementById('violeta')
+const naranja = document.getElementById('naranja')
+const verde = document.getElementById('verde')
+
+const btnEmpezar = document.getElementById('btnEmpezar')
+
+class game {
+    constructor(){
+        this.initialize()
+    }
+
+    initialize() {
+        btnEmpezar.classList.add('hide')
+
+    }
+}
+
+function startGame(){
+alert("The game is about to start")
+var game = new Game()
+}
