33f2bf0571ac842a1bd736933b215e5f1366e274
Catherine <katiiforever@gmail.com>
2019-06-21 21:45:43 -0400

    iluminating colors

--- script.js
+++ script.js
@@ -1,28 +1,62 @@
-const celeste = document.getElementById('celeste')
-const violeta = document.getElementById('violeta')
-const naranja = document.getElementById('naranja')
-const verde = document.getElementById('verde')
+const blue = document.getElementById('blue')
+const violet = document.getElementById('violet')
+const orange = document.getElementById('orange')
+const green = document.getElementById('green')
 const btnEmpezar = document.getElementById('btnEmpezar')
 
 class Game {
     constructor() {
         this.initialize()
         this.generateSequence()
+        this.nextLevel()
     }
 
     initialize() {
         btnEmpezar.classList.add('hide')
         this.level = 1
         this.colors = {
-            celeste, 
-            violeta,
-            naranja,
-            verde
+            blue, 
+            violet,
+            orange,
+            green
         }
     }
 
     generateSequence() {
         this.sequence = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
+    }
+
+    nextLevel(){
+        this.iluminateSequence()
+    }
+
+    transformNumber(number){
+        switch(number) {
+            case 0:
+                return 'blue'
+            case 1:
+                return 'violet'
+            case 2:
+                return 'orange'
+            case 3: 
+                return 'green'
+        }
+    }
+
+    iluminateSequence(){
+        for (let i = 0; i < this.level; i++){
+            const color = this.transformNumber(this.sequence[i])
+            setTimeout(() => this.iluminateColor(color), 1000 * i) 
+        }
+    }
+
+    iluminateColor(color){
+        this.colors[color].classList.add('light')
+        setTimeout(() => this.turnOffColor(color), 350)
+    }
+
+    turnOffColor(color){
+        this.colors[color].classList.remove('light')
     }
 }
 
