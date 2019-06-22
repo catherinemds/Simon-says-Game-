888e96aabf45a2534a0cda6995d4b6c79fbecd65
Catherine <katiiforever@gmail.com>
2019-06-21 20:44:43 -0400

    generating sequence

--- script.js
+++ script.js
@@ -4,18 +4,29 @@ const naranja = document.getElementById('naranja')
 const verde = document.getElementById('verde')
 const btnEmpezar = document.getElementById('btnEmpezar')
 
-class game {
+class Game {
     constructor() {
         this.initialize()
+        this.generateSequence()
     }
 
     initialize() {
         btnEmpezar.classList.add('hide')
+        this.level = 1
+        this.colors = {
+            celeste, 
+            violeta,
+            naranja,
+            verde
+        }
+    }
 
+    generateSequence() {
+        this.sequence = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
     }
 }
 
 function startGame(){
-alert("The game is about to start")
-var game = new Game()
+   window.game = new Game()
+
 }
